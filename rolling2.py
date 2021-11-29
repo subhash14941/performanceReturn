903509fc88e08a83612de9a400d83345e0a76d48
subhash.s.hundi@gmail.com <root@LAPTOP-44CFO6SR.localdomain>
2021-11-29 15:11:58 +0530

    average

--- rolling2.py
+++ rolling2.py
@@ -40,7 +40,9 @@ lastDate=pd.to_datetime(retDf.tail(1)['pd_date'].values[0]).strftime('%Y%m%d')
 allStrats=list(retDic.keys())
 for strat in allStrats:
     retDf['rolling7'+strat]=retDf[strat].rolling(7).sum()
+    retDf['average7'+strat]=retDf['rolling7'+strat]/7
     retDf['rolling14'+strat]=retDf[strat].rolling(14).sum()
+    retDf['average14'+strat]=retDf['rolling14'+strat]/7
 retDf['date']=retDf.index
 col1, col2 = st.columns(2)
 stratName=col1.selectbox('Select a Strategy',tuple(allStrats[:1]+["All"]+allStrats[1:]))
@@ -48,5 +50,5 @@ stratName=col1.selectbox('Select a Strategy',tuple(allStrats[:1]+["All"]+allStra
 timePeriod=col2.selectbox('Time Period',(7,14))
 w1=650;w2=1200
 h1=600;h2=600
-fig=px.line(retDf,x="date",y=f'rolling{timePeriod}{stratName}',title=stratName+' '+str(timePeriod)+' days (% Returns)',width=w2, height=h2)
+fig=px.line(retDf,x="date",y=[f'rolling{timePeriod}{stratName}',f'average{timePeriod}{stratName}'],title=stratName+' '+str(timePeriod)+' days (% Returns)',width=w2, height=h2)
 col1.plotly_chart(fig)
